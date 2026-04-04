efb597d8edfd0961fbb9c4b0218f406860091159
AvhiMaz <avhimazumder5@outlook.com>
2026-04-04 19:36:27 +0530

    feat: impl shared struct
    Signed-off-by: AvhiMaz <avhimazumder5@outlook.com>

--- sfp-common/src/lib.rs
+++ sfp-common/src/lib.rs
@@ -1,1 +1,11 @@
 #![no_std]
+
+#[repr(C)]
+#[derive(Clone, Copy)]
+pub struct LatencyEvent {
+    pub pid: u32,
+    pub latency_ns: u64,
+}
+
+#[cfg(feature = "user")]
+unsafe impl aya::Pod for LatencyEvent {}
