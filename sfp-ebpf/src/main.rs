5f35f4cb41944a01b843236caf654c62ddb60fad
AvhiMaz <avhimazumder5@outlook.com>
2026-04-07 10:08:19 +0530

    feat: impl fentry hook
    impl a fn that gonna read pid and start time
    and gonna push it to `HashMap`. (hashmap is <u32, u64> where u32 is the pid and u64 is the timestamp)
    
    some sources:
    
    - https://github.com/torvalds/linux/blob/master/include/uapi/linux/bpf.h#L5908
    - https://github.com/torvalds/linux/blob/master/kernel/bpf/helpers.c#L178
    
    Signed-off-by: AvhiMaz <avhimazumder5@outlook.com>

--- sfp-ebpf/src/main.rs
+++ sfp-ebpf/src/main.rs
@@ -2,30 +2,23 @@
 #![no_main]
 
 use aya_ebpf::{
+    EbpfContext,
+    helpers::generated::bpf_ktime_get_ns,
     macros::fentry,
     maps::{HashMap, RingBuf},
     programs::FEntryContext,
 };
-use aya_log_ebpf::info;
 
+#[map]
 static START_TIME: HashMap<u32, u64> = HashMap::with_max_entries(10240, 0);
+
+#[map]
 static EVENTS: RingBuf = RingBuf::with_byte_size(4096 * 1024, 0);
 
 #[fentry(function = "vfs_read")]
-pub fn sfp(ctx: FEntryContext) -> u32 {
-    match try_sfp(ctx) {
-        Ok(ret) => ret,
-        Err(ret) => ret,
-    }
-}
-
-fn try_sfp(ctx: FEntryContext) -> Result<u32, u32> {
-    info!(&ctx, "function vfs_read called");
-    Ok(0)
-}
-
-#[cfg(not(test))]
-#[panic_handler]
-fn panic(_info: &core::panic::PanicInfo) -> ! {
-    loop {}
+pub fn vf_read_start(ctx: FEntryContext) -> u32 {
+    let pid = ctx.pid();
+    let ts = unsafe { bpf_ktime_get_ns() };
+    START_TIME.insert(&pid, &ts, 0);
+    0
 }
